25eadc3a975cf5a726c4d5ecbdc1a666d437059f
Natalie Popescu <nataliepopescu@gmail.com>
2024-02-28 17:20:05 -0500

    additions for rust import

--- app/src/main/java/com/example/minimalrustimport/MainActivity.kt
+++ app/src/main/java/com/example/minimalrustimport/MainActivity.kt
@@ -8,4 +8,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    // still don't entirely understand why we are adding this here though
+    // furthermore, this is the first change I've made to the code and it still compiles
+    companion object {
+        init {
+            System.loadLibrary("tank")
+        }
+    }
 }
